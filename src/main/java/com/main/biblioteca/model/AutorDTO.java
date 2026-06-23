a3826786a277439525daa39b1aa8d7ccea11a900
Beatrice <beatricesilva1113@gmail.com>
2026-06-23 16:46:57 -0300

    first commit

---
+++ src/main/java/com/main/biblioteca/model/AutorDTO.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.main.biblioteca.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+/**
+ *
+ * @author Aluno
+ */
+@Entity
+@Table(name = "autor")
+public class AutorDTO {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+}
